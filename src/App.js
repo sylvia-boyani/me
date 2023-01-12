7ff3dececb96b34f468ac9af68cb322d56216868
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-01-12 18:56:03 +0000

    add pages folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Nav from "./components/Nav"
 import Home from "./components/Home"
 
 function App() {
   return (
     <div>
-      <Nav />
-      <Home />
+     <Home />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Home.css';
+import Nav from '../components/Nav'
 
 
 function Home() {
   return (
     <div className='home'>
+       <Nav />
+     <div className='content'>
      <h1>Be A Digital Innovator</h1>
      <p>Digital technologies are disrupting 
-        old industries and creating new ones. 
+        old industries and creating new ones. <br></br>
         This brings lots of opportunities, but 
         challenges as well. Are you ready for them?</p> 
+     </div>
     </div>
   );
 }
