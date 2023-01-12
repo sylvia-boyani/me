a3afa9c69541be90dafdfcdd5fde1466825f6dd0
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-01-12 22:38:54 +0000

    done with aboutMe section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './AboutMe.css'
 import img from '../assets/img1.jpg'
+import im3 from '../assets/im3.jpg'
+import im4 from '../assets/im4.jpg'
 
 function AboutMe() {
     return (
@@ -14,6 +16,31 @@ function AboutMe() {
             We all have our strengths. I’ve perfected 
             mine <br></br>so you can focus on yours. Get in touch 
             to find <br></br>out how I can help.</p>
+            </div>
+
+           
+         <div className='about-content'>
+         <p><h3>My Values</h3>
+         My business is more than just a job. 
+         Each time I take on a new project, 
+         I make sure it aligns with my core 
+         set of values so I know I can deliver great results.
+         My curiosity makes me love learning, and added with my 
+         perfectionism, I am able to come up with quality work.</p>
+            </div>
+            <div className='about-img'>
+               <img  src={im3} alt="about us" />
+            </div>
+
+            <div className='about-img'>
+               <img  src={im4} alt="about us" />
+            </div>
+         <div className='about-content'>
+         <p>
+            <h3>My Clients</h3>
+            Because I'm experienced, reliable, 
+            and focused on results, I've had the 
+            privilege of working with a host of fantastic clients.</p>
             </div>
             
         </div>
