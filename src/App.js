0ef3f12ee00210c8cf884a8d67ea4672891a5ec9
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-01-12 19:44:09 +0000

    delete public folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutMe from './components/AboutMe';
 import Home from "./components/Home"
 
 function App() {
   return (
     <div>
      <Home />
+     <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css'
+
+function AboutMe() {
+    return(
+        <div className='me'>
+         <img src={img3.jpg} alt="about us" />
+         <p>We all have our strengths. I’ve perfected 
+            mine so you can focus on yours. Get in touch <br></br>
+            to find out how I can help.</p>
+        </div>
+    )
+}
+
+export default AboutMe;
