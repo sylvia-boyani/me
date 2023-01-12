bd85870673b6148931d57e3e72a8731dfb01113a
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-01-12 16:32:18 +0000

    done with navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className='home'>
+     <h1>Be A Digital Innovator</h1>
+     <p>Digital technologies are disrupting 
+        old industries and creating new ones. 
+        This brings lots of opportunities, but 
+        challenges as well. Are you ready for them?</p> 
+    </div>
+  );
+}
+
+export default Home;
