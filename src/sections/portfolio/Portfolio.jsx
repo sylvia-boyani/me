56d08a9f923385e7b9ba4026239f8df2aa598fb8
Boyani <sylviaboyani11@gmail.com>
2023-02-08 00:56:45 +0000

    add portfolio section

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,9 +1,165 @@
-import React from 'react'
-import './portfolio.css'
+import React,{Fragment,useState} from 'react'
+import "./portfolio.css"
+import Menu from './portdata'
 
-function Portfolio() {
+const Portfolio = () => {
+	      
+	
+	const[items, setItems] = useState(Menu)
+
+	const filterItem = (categItem) =>{
+		const updatedItems = Menu.filter((cureElem) =>{
+			return cureElem.category === categItem;
+		})
+		setItems(updatedItems);
+	}
+
   return (
-    <section id='portfolio'><h2>Portfolio</h2></section>
+    <Fragment>
+
+    <section className="portfolio" id='portfolio'>
+
+    <div className='port-tittle'>
+		<h2>my <span>works</span></h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, temporibus consequuntur dicta ullam illo facere.</p>
+	    <div className="port-line"></div>
+	</div>
+
+    <div class="nav">
+		<ul class="nav-list">
+			<li onClick={() => setItems(Menu)}>
+				<a class="img-filter active">All</a>
+			</li>
+			<li onClick={() => filterItem('Designing')}>
+				<a class="img-filter">Designing</a>
+			</li>
+			<li onClick={() => filterItem('frontend')}>
+				<a class="img-filter">frontend</a>
+			</li>
+			<li onClick={() => filterItem('Backend')}>
+				<a class="img-filter" >Backend</a>
+			</li>
+			<li onClick={() => filterItem('Seo')}>
+				<a class="img-filter" >Seo</a>
+			</li>
+		</ul>
+		</div>
+
+      <div className="port-row">
+
+	  {
+		items.map((elem)=>{
+
+         return(
+
+	<div className="port-col" key={elem.id}>
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={elem.image}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>{elem.heading}</h4>
+						<p>{elem.paragrapgh}</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div>
+
+         )
+
+		})
+	  }
+
+    {/* <div className="port-col">
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={images}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>Work One</h4>
+						<p>Brand</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div> */}
+
+    {/* <div className="port-col">
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={images}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>Work One</h4>
+						<p>Brand</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div> */}
+
+    {/* <div className="port-col">
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={images}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>Work One</h4>
+						<p>Brand</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div> */}
+
+    {/* <div className="port-col">
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={images}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>Work One</h4>
+						<p>Brand</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div> */}
+{/* 
+    <div className="port-col">
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={images}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>Work One</h4>
+						<p>Brand</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div> */}
+
+    {/* <div className="port-col">
+        <div class="project-item">
+			<a class="zoom1" href="#">
+              <img alt="" src={images}/>
+				<div class="overlay">
+					<div class="overlay-inner">
+						<h4>Work One</h4>
+						<p>Brand</p>
+					</div>
+				</div>
+              </a>
+		</div>
+    </div> */}
+
+    </div>
+
+    </section>
+
+    </Fragment>
   )
 }
 
---
+++ src/sections/portfolio/portdata.jsx
@@ -0,0 +1,45 @@
+const Menu = [
+    {
+        id: 1,
+        image:"../../",
+        heading:"Portfolio",
+        paragrapgh:"Website",
+        category:"frontend"
+    },
+    {
+        id: 2,
+        image:"PortImages/pexels-cottonbro-studio-4709289.jpg",
+        heading:"Business",
+        paragrapgh:"Website",
+        category:"frontend"
+    },
+    {
+        id: 3,
+        image:"PortImages/pexels-picjumbocom-196644.jpg",
+        heading:"Business",
+        paragrapgh:"Card",
+        category:"Designing"
+    },
+    {
+        id: 4,
+        image:"PortImages/pexels-olia-danilevich-4974912.jpg",
+        heading:"E-Commerce",
+        paragrapgh:"API",
+        category:"Backend"
+    },
+    {
+        id: 5,
+        image:"PortImages/pexels-moose-photos-1037993.jpg",
+        heading:"Website",
+        paragrapgh:"Design",
+        category:"Designing"
+    },
+    {
+        id: 6,
+        image:"PortImages/digital-marketing-gcef55a09c_1280.jpg",
+        heading:"Search Engine",
+        paragrapgh:"Optimiazation",
+        category:"Seo"
+    },
+]
+export default Menu
